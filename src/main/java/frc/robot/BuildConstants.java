119a84931edefb9386e538fdb3c4ebf9427998a5
Andrew Rickard <arickard@salesforce.com>
2025-01-25 23:42:32 -0500

    taming build

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,19 +1,17 @@
 package frc.robot;
 
-/**
- * Automatically generated file containing build version information.
- */
+/** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025NashobaRoboticsKitBot";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = -1;
-  public static final String GIT_SHA = "UNKNOWN";
-  public static final String GIT_DATE = "UNKNOWN";
-  public static final String GIT_BRANCH = "UNKNOWN";
-  public static final String BUILD_DATE = "2025-01-25 14:38:42 EST";
-  public static final long BUILD_UNIX_TIME = 1737833922446L;
-  public static final int DIRTY = 129;
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "e86f48c8a6e627139c9a88b783479a870f00a5d5";
+  public static final String GIT_DATE = "2025-01-25 16:08:44 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-01-25 23:41:10 EST";
+  public static final long BUILD_UNIX_TIME = 1737866470876L;
+  public static final int DIRTY = 1;
 
-  private BuildConstants(){}
+  private BuildConstants() {}
 }
